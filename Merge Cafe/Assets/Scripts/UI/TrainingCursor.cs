80c96a9c907741eb77523cbb32c817632e82ad3a
nech <dhdhhd.cx@gmail.com>
2022-12-14 15:41:21 +0500

    Fixed TrainingCursor

--- Merge Cafe/Assets/Scripts/UI/TrainingCursor.cs
+++ Merge Cafe/Assets/Scripts/UI/TrainingCursor.cs
@@ -1,53 +1,52 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
 using DG.Tweening;
 using UnityEngine;
 
 public class TrainingCursor : MonoBehaviour
 {
-    public GameObject cursor;
-    private Sequence tween;
-
-    // Start is called before the first frame update
+    [SerializeField] private GameObject cursor;
+    [SerializeField] private float defaultScale;
+    [SerializeField] private float clickScale;
+    [SerializeField] private float speed;
+    private Sequence _tween;
+    
     public void ClickAnimation(Vector3 startPoint, Vector3 endPoint)
     {
-        tween.Kill();
+        _tween.Kill();
         cursor.SetActive(true);
         var target = cursor.transform;
-        tween = DOTween.Sequence().Append(target.DOLocalMove(endPoint, 1).From(startPoint))
-            .AppendInterval(0.5f).Append(target.DOScale(11f, 0.5f))
-            .Append(target.DOScale(15, 0.5f))
+        _tween = DOTween.Sequence().Append(target.DOLocalMove(endPoint, 1).From(startPoint))
+            .AppendInterval(speed).Append(target.DOScale(clickScale, speed))
+            .Append(target.DOScale(defaultScale, speed))
             .SetLoops(-1);
     }
 
     public void DragAnimation(Vector3 startPoint, Vector3 pickPoint, Vector3 endPoint)
     {
-        tween.Kill();
+        _tween.Kill();
         cursor.SetActive(true);
         var target = cursor.transform;
-        tween = DOTween.Sequence().Append(target.DOLocalMove(pickPoint, 1).From(startPoint))
-            .AppendInterval(0.5f).Append(target.DOScale(11f, 0.5f))
-            .Append(target.DOLocalMove(endPoint, 2))
-            .Append(target.DOScale(15, 0.5f))
+        _tween = DOTween.Sequence().Append(target.DOLocalMove(pickPoint, 1).From(startPoint))
+            .AppendInterval(speed).Append(target.DOScale(clickScale, speed))
+            .Append(target.DOLocalMove(endPoint, speed))
+            .Append(target.DOScale(defaultScale, speed))
             .SetLoops(-1);
     }
 
     public void Stop()
     {
         cursor.SetActive(false);
-        tween.Pause();
+        _tween.Pause();
     }
 
     public void Restart()
     {
-        tween.Restart();
+        _tween.Restart();
         cursor.SetActive(true);
     }
 
     public void Kill()
     {
         cursor.SetActive(false);
-        tween.Kill();
+        _tween.Kill();
     }
 }
